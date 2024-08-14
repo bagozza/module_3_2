2d54ae4436b4caea65f43b924c188bbf8d1a94f9
bagozza <bagozza_96@mail.ru>
2024-08-14 18:45:18 +0500

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -5,4 +5,8 @@ def print_params(a=1, b='строка', c=True):
 values_list = [7, True, 'A']
 values_dict = {'a': 9, 'b': True, 'c': 'False'}
 values_list_2 = [54.32, 'Строка']
+print_params()
+print_params(b=25, c=[1, 2, 3])
+print_params(**values_dict)
+print_params(*values_list)
 print_params(*values_list_2, 42)
